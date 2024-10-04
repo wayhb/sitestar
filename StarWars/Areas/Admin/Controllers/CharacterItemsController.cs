33b7e75eaac89b3e72cdaefdc2cb311646299d50
wayhb <adely5mukomashina@gmail.com>
2024-10-05 01:22:34 +0300

    убрать редактирование меню

---
+++ StarWars/Areas/Admin/Controllers/CharacterItemsController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StarWars.Domain;
+using StarWars.Domain.Entities;
+
+namespace StarWars.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class CharacterItemsController : Controller
+	{
+		private readonly DataManager dataManager;
+	    private readonly IWebHostEnvironment hostingEnvironment;
+		public CharacterItemsController(DataManager dataManager, IWebHostEnvironment hostingEnvironment)
+		{
+			this.dataManager = dataManager;
+			this.hostingEnvironment = hostingEnvironment;
+		}
+
+		public ActionResult Edit(Guid id)
+		{
+			var entity = id == default ? new CharacterItem() : dataManager.CharacterItems.GetCharacterItemById(id);
+			return View(entity);
+		}
+
+		[HttpPost]
+		public ActionResult Edit(CharacterItem model, IFormFile	titleImageFile)
+		{
+			if(ModelState.IsValid)
+			{
+				if(titleImageFile != null)
+				{
+					model.TitleImagePath = titleImageFile.FileName;
+					using(var stream = new FileStream(Path.Combine(hostingEnvironment.WebRootPath,
+						"images/", titleImageFile.FileName), FileAccess))
+					{
+						titleImageFile.CopyTo(stream);
+					}
+
+				}
+				dataManager.CharacterItems.SaveCharacterItem(model);
+				return RedirectToAction(nameof(HomeController.Index), nameof(HomeController).CutController());
+			}
+			return View(model);
+		}
+
+		[HttpPost]
+		public ActionResult Delete(Guid id)
+		{
+			dataManager.CharacterItems.DeleteCharacterItem(id);
+			return RedirectToAction(nameof(HomeController.Index), nameof(HomeController).CutController());
+
+		}
+
+
+	}
+}
--- StarWars/Areas/Admin/Controllers/HomeController.cs
+++ StarWars/Areas/Admin/Controllers/HomeController.cs
@@ -1,14 +1,23 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using StarWars.Domain;
 
 namespace StarWars.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class HomeController : Controller
     {
+        // чтобы был доступ к нашим доменным объектам
+        private readonly DataManager dataManager;   
+
+        public HomeController(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
         public ActionResult Index()
         {
-            return View();
+            //вывод всех услуг на главную страницу админа
+            return View(dataManager.CharacterItems.GetCharactersItems());
         }
 
     }
